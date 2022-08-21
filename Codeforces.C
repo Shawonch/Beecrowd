9934b9f19e5449d5272e92a884eeb00248221e57
Shawonch <109814994+Shawonch@users.noreply.github.com>
2022-08-21 14:11:05 +0600

    Create Codeforces.C

---
+++ Codeforces.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+ long long int X,Y,summation,multiplication,subtraction;
+scanf("%lld%lld",&X,&Y);
+printf("%lld + %lld = %lld\n",X,Y,X+Y);
+printf("%lld * %lld = %lld\n",X,Y,X*Y);
+printf("%lld - %lld = %lld\n",X,Y,X-Y);
+return 0;
+return 0;
+}
